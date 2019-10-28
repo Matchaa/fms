b975ba853099cc31b7c83f886f9b0c74acb786fb
汪忠祥 <410805964@qq.com>
2019-10-28 21:18:04 +0800

    添加

--- src/views/deliverNote/components/addDialog.vue
+++ src/views/deliverNote/components/addDialog.vue
@@ -1,84 +1,124 @@
 <!--添加送货单-->
 <template>
-  <el-dialog title="新增送货单"
-    :visible.sync="show"
-    width="1000px"
-    @open="dialogOpen">
-    <el-form class="form"
-      :inline="true">
+  <el-dialog title="新增送货单" :visible.sync="show" width="1000px" @open="dialogOpen">
+    <el-form class="form" :inline="true">
       <el-form-item label="状态">
-        <el-select v-model="form.state"
-          placeholder="请选择状态">
-          <el-option label="未打印"
-            :value="0"></el-option>
-          <el-option label="未发货"
-            :value="1"></el-option>
-          <el-option label="已发货"
-            :value="2"></el-option>
+        <el-select v-model="form.state" placeholder="请选择状态">
+          <el-option label="未打印" :value="0"></el-option>
+          <el-option label="未发货" :value="1"></el-option>
+          <el-option label="已发货" :value="2"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="客户名称">
-        <el-select v-model="form.name"
-          filterable
-          placeholder="请选择客户"
-          @change="customerChange">
-          <el-option v-for="customer in customerList"
-            :key="customer.id"
-            :label="customer.name"
-            :value="customer.id"></el-option>
+        <el-select v-model="form.name" filterable placeholder="请选择客户" @change="customerChange">
+          <el-option v-for="customer in customerList" :key="customer.id" :label="customer.name" :value="customer.id"></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="客户地址">
-        <el-input v-model="form.address"
-          disabled
-          placeholder="请输入客户地址"></el-input>
+        <el-input v-model="form.address" disabled placeholder="请输入客户地址"></el-input>
       </el-form-item>
       <el-form-item label="联系电话">
-        <el-input v-model="form.phone"
-          disabled
-          placeholder="请输入联系电话"></el-input>
+        <el-input v-model="form.phone" disabled placeholder="请输入联系电话"></el-input>
       </el-form-item>
       <el-form-item label="送货时间">
-        <el-date-picker v-model="form.date"
-          type="date"
-          placeholder="请选择送货日期"
-          @change="dateChange">
+        <el-date-picker v-model="form.date" type="date" placeholder="请选择送货日期" @change="dateChange">
         </el-date-picker>
       </el-form-item>
     </el-form>
-    <el-button type="primary"
-      @click="addGoods">添加</el-button>
-    <el-table class="table"
-      :data="tableData">
-      <el-table-column prop="name"
-        label="产品名称"
-        width="150">
+    <el-button type="primary" @click="addGoods">添加</el-button>
+    <el-table class="table" :data="tableData">
+      <el-table-column label="产品名称" width="200">
         <template slot-scope="scope">
-          <el-select v-model="scope.row.name"
-            filterable
-            placeholder="请选择钢种">
-            <el-option v-for="product in productList"
-              :key="product.id"
-              :label="product.name"
-              :value="product.id"></el-option>
+          <el-select v-model="scope.row.productName" filterable placeholder="请选择产品名称" @change="(value)=>productChange(value,scope)" size="small">
+            <el-option v-for="item in productList" :key="item.id" :label="item.name" :value="item.id"></el-option>
           </el-select>
         </template>
       </el-table-column>
+      <el-table-column v-for="(header,key) in tableHeader" :key="key" :prop="key" :label="header.title" :width="header.width||150">
+        <template slot-scope="scope">
+          <!-- <el-select v-if="header.type==='select'" v-model="scope.row[key]" filterable :placeholder="'请选择'+header.title" @change="(value)=>header.change(value,scope.row)">
+            <el-option v-for="item in header.list" :key="item.id" :label="item.name" :value="item.id"></el-option>
+          </el-select> -->
+          <el-input v-if="header.type==='input'" v-model="scope.row[key]" :disabled="header.disabled||scope.row.productType===''||false" :placeholder="'请输入'+header.title" @blur="(value)=>header.change(value,scope)" size="small"></el-input>
+          <el-input-number v-else-if="header.type==='inputNumber'" v-model="scope.row[key]" :disabled="header.disabled||scope.row.productType===''||false" :precision="header.precision||0" :step="header.step||1" :min="header.min||0" size="small"></el-input-number>
+        </template>
+      </el-table-column>
+      <el-table-column fixed="right" label="操作" width="80">
+        <template slot-scope="scope">
+          <el-button @click.native.prevent="deleteRow(scope.$index, scope.row)" size="small" type="danger">
+            <i class="el-icon-delete table__delete"></i>
+          </el-button>
+        </template>
+      </el-table-column>
     </el-table>
-
-    <span slot="footer"
-      class="dialog-footer">
+    <div class="meta">备注：净尺寸和毛尺寸规格参考格式：100*100*100。</div>
+    <span slot="footer" class="dialog-footer">
       <el-button @click="show = false">取 消</el-button>
-      <el-button type="primary"
-        @click="onCertain">确 定</el-button>
+      <el-button type="primary" @click="onCertain">确 定</el-button>
     </span>
   </el-dialog>
 </template>
 
 <script>
 import moment from 'moment'
+
 export default {
   data() {
+    var generalStandardsChange = (value, scope) => {
+      const { $index: index, row } = scope
+      // console.log(row)
+      const general = row.generalStandards.split('*')
+      if (general.length !== 3) {
+        this.$message.warning('请按照参考格式输入净尺寸规格！')
+        row.generalStandards = ''
+        this.$set(this.tableData, index, row)
+        return
+      }
+      let isAccord = general.every(item => {
+        return item >= 0
+      })
+      if (!isAccord) {
+        this.$message.warning('净尺寸规格只能为大于等于0的数字！')
+        row.generalStandards = ''
+        this.$set(this.tableData, index, row)
+        return
+      }
+      if (row.additionalStandards === '') return
+      const additional = row.additionalStandards.split('*')
+      console.log(general
+        .map((item, index) => {
+          return Number(item)+Number(additional[index]) 
+        }))
+      const sum = general
+        .map((item, index) => {
+          return Number(item)+Number(additional[index]) 
+        })
+        .reduce((accumulator, currentValue) => accumulator * currentValue)
+
+      row.weight = (sum * 7.85 * 0.000001 * row.amount).toFixed(2)
+      this.$set(this.tableData, index, row)
+    }
+    var additionalStandardsChange = (value, scope) => {
+      const { $index: index, row } = scope
+      // console.log(row)
+      const additional = row.additionalStandards.split('*')
+      console.log(additional)
+      if (additional.length !== 3) {
+        row.additionalStandards = ''
+        this.$set(this.tableData, index, row)
+        this.$message.warning('请按照参考格式输入毛尺寸规格！')
+        return
+      }
+      let isAccord = additional.every(item => {
+        return item >= 0
+      })
+      if (!isAccord) {
+        row.additionalStandards = ''
+        this.$set(this.tableData, index, row)
+        this.$message.warning('毛尺寸规格只能为大于等于0的数字！')
+        return
+      }
+    }
     return {
       show: false,
       form: {
@@ -89,63 +129,92 @@ export default {
         date: ''
       },
       customerList: [],
-      tableHeader: [
-        {
-          title: '产品名称',
-          name: 'name'
-        },
-        {
+      tableHeader: {
+        // productName: {
+        //   title: '产品名称',
+        //   type: 'select',
+        //   list: [],
+        //   change:this.productChange
+        // },
+        generalStandards: {
           title: '净尺寸规格',
-          name: 'generalStandards',
-          width: 150
+          type: 'input',
+          change: generalStandardsChange
         },
-        {
+        additionalStandards: {
           title: '毛尺寸规格',
-          name: 'additionalStandards',
-          width: 150
+          type: 'input',
+          change: additionalStandardsChange
         },
-        {
+        amount: {
           title: '件数',
-          name: 'amount'
+          type: 'inputNumber',
+          precision: 0
         },
-        {
+        weight: {
           title: '重量',
-          name: 'weight'
+          name: 'weight',
+          type: 'input',
+          disabled: true
         },
-        {
+        unitPrice: {
           title: '单价',
-          name: 'unitPrice'
+          name: 'unitPrice',
+          precision: 2,
+          type: 'inputNumber'
         },
-        {
+        price: {
+          title: '金额',
+          name: 'price',
+          type: 'input',
+
+          disabled: true
+        },
+        material: {
           title: '材料费',
-          name: 'material'
+          name: 'material',
+          precision: 2,
+          type: 'inputNumber'
         },
-        {
+        machining: {
           title: '精加工',
-          name: 'machining'
+          name: 'machining',
+          precision: 2,
+          type: 'inputNumber'
         },
-        {
+        flashSide: {
           title: '飞边',
-          name: 'flashSide'
+          name: 'flashSide',
+          precision: 2,
+          type: 'inputNumber'
         },
-        {
+        gasCut: {
           title: '气割费',
-          name: 'gasCut'
+          name: 'gasCut',
+          precision: 2,
+          type: 'inputNumber'
         },
-        {
+        saw: {
           title: '锯费',
-          name: 'saw'
+          name: 'saw',
+          precision: 2,
+          type: 'inputNumber'
         },
-        {
+        totalPrice: {
           title: '总金额',
-          name: 'totalPrice'
+          name: 'totalPrice',
+          type: 'input',
+          disabled: true
         },
-        {
+        remarks: {
           title: '备注',
-          name: 'remarks'
+          name: 'remarks',
+          type: 'input',
+          width:200
         }
-      ],
-      tableData: []
+      },
+      tableData: [],
+      productList: []
     }
   },
 
@@ -156,7 +225,8 @@ export default {
   methods: {
     addGoods() {
       this.tableData.unshift({
-        name: '',
+        productName: '',
+        productType: '',
         generalStandards: '',
         additionalStandards: '',
         amount: 1,
@@ -173,6 +243,16 @@ export default {
     },
     dialogOpen() {
       this.reFindCustomerList()
+      this.reFindProductList().then(() => {
+        this.addGoods()
+      })
+    },
+    productChange(value, scope) {
+      const { $index: index, row } = scope
+      row.productType = this.productList.find(item => {
+        return item.id === value
+      }).typeId
+      this.$set(this.tableData, index, row)
     },
     customerChange(value) {
       const selected = this.customerList.find(item => item.id === value)
@@ -188,7 +268,7 @@ export default {
       })
     },
     reFindProductList() {
-      this.$db.getData('PRODUCT_DATA').then(res => {
+      return this.$db.getData('PRODUCT_DATA').then(res => {
         this.productList = res
       })
     },
@@ -209,5 +289,12 @@ export default {
 <style lang='scss' scoped>
 .table {
   width: 100%;
+  &__delete {
+    color: #fff;
+  }
+}
+.meta {
+  color: #a0a0a0;
+  margin-top: 10px;
 }
 </style>
